d77987d8a0a98b75a5141a0260fc772fb8c43a8e
Xiang Yuan <xiangyuan.poon@wizvision.com>
2024-11-01 17:08:26 +0800

    Fix Daphne ASGI Pointing to Incorrect Settings

--- Django_WebSocket_Backend/django_websocket_project/django_websocket_project/asgi.py
+++ Django_WebSocket_Backend/django_websocket_project/django_websocket_project/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from property_websocket_app.routing import websocket_urlpatterns  # This import should be fine
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_websocket_project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
