e2bb29746e3655f927360ab488c12a1ce6349648
Xiang Yuan <xiangyuan.poon@wizvision.com>
2024-11-04 17:55:46 +0800

    Prototype for JWT implemented with Websocket

--- Django_WebSocket_Backend/django_websocket_project/property_websocket_app/consumers.py
+++ Django_WebSocket_Backend/django_websocket_project/property_websocket_app/consumers.py
@@ -1,24 +1,63 @@
-# property_websocket_app/consumers.py
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
+from urllib.parse import parse_qs
+import jwt
+from django.conf import settings
+import logging
 
+# Configure logging for debugging
+logger = logging.getLogger(__name__)
 
 class BidConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print("WebSocket connection attempt")
-        from property_http_app.models import Property
-        from property_http_app.serializers import BidSerializer
-        self.property_id = self.scope['url_route']['kwargs']['property_id']
-        self.room_group_name = f'bid_{self.property_id}'
+        from django.contrib.auth.models import User
+        logger.info("WebSocket connection attempt")
 
-        # Join room group
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        await self.accept()
+        query_string = self.scope['query_string'].decode()
+        query_params = parse_qs(query_string)
+        token = query_params.get('token', [None])[0]
+
+        if token is None:
+            logger.warning("No token provided, rejecting connection")
+            await self.close(code=4003)
+            return
+
+        logger.info(f"Token received: {token}")
+
+        try:
+            # Decode JWT token
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+            logger.info(f"Token payload: {payload}")
+
+            # Fetch user by ID in payload
+            user = await sync_to_async(User.objects.get)(id=payload['user_id'])
+            self.scope['user'] = user
+            logger.info(f"User authenticated: {user.username}")
+
+            # Set property_id and room_group_name
+            self.property_id = self.scope['url_route']['kwargs']['property_id']
+            self.room_group_name = f'bid_{self.property_id}'
+
+            # Join room group and accept connection
+            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+            await self.accept()
+
+        except jwt.ExpiredSignatureError:
+            logger.error("Token expired")
+            await self.close(code=4001)
+        except jwt.InvalidTokenError:
+            logger.error("Invalid token")
+            await self.close(code=4002)
+        except User.DoesNotExist:
+            logger.error("User does not exist")
+            await self.close(code=4004)
 
     async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+         # Check if room_group_name was set
+        if hasattr(self, 'room_group_name'):
+            # Leave room group
+            await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         from property_http_app.models import Property
--- Django_WebSocket_Backend/django_websocket_project/property_websocket_app/routing.py
+++ Django_WebSocket_Backend/django_websocket_project/property_websocket_app/routing.py
@@ -4,5 +4,4 @@ from .consumers import BidConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/bid/(?P<property_id>[^/]+)/$', BidConsumer.as_asgi()),
-    path('ws/test/', BidConsumer.as_asgi()),
 ]
