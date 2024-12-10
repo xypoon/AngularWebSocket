c650da529d1516107fcc6adbfffafba05bc5b39b
Xiang Yuan <xiangyuanpoon@gmail.com>
2024-12-10 16:08:25 +0800

    BYPASS Auth Angular & Locust Bat File

--- Angular_WebSocket_Frontend/src/app/auth.guard.ts
+++ Angular_WebSocket_Frontend/src/app/auth.guard.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './services/auth.service'; // Ensure you have an AuthService
+import { environment } from '../environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(): boolean {
-    console.log('AuthGuard#canActivate called:', this.authService.isAuthenticated());
-    if (this.authService.isAuthenticated()) {
+    if (environment.bypassAuth) {
+      console.log('AuthGuard#canActivate called: BYPASSING AUTH');
       return true;
     } else {
-      this.router.navigate(['/login']);
-      return false;
+      console.log('AuthGuard#canActivate called:', this.authService.isAuthenticated());
+      if (this.authService.isAuthenticated()) {
+        return true;
+      } else {
+        this.router.navigate(['/login']);
+        return false;
+      }
     }
   }
 }
---
+++ Angular_WebSocket_Frontend/src/environment/environment.ts
@@ -0,0 +1,5 @@
+// environment.ts
+export const environment = {
+    production: false,
+    bypassAuth: true // Set this to true to bypass auth in development
+};
